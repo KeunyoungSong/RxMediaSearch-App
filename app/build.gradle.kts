1f61ea2b77458fce60fe26f7334dde4dcd45b999
LB-Brandon <rmsdud623@gmail.com>
2024-05-28 17:56:08 +0900

    Retrofit 로거 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,11 +49,17 @@ dependencies {
 	implementation("com.squareup.okhttp3:okhttp:4.12.0")
 	implementation("com.squareup.retrofit2:retrofit:2.11.0")
 	implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+	implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+	
 	implementation("io.coil-kt:coil:2.4.0")
-	implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+	
 	implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
 	implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
+	implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+	
 	implementation("androidx.fragment:fragment-ktx:1.7.1")
+	
+	
 	
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
