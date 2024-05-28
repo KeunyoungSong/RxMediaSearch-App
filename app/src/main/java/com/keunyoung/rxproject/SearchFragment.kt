94b755bc0a2a6f36609f5987f41f57cab0c614a0
LB-Brandon <rmsdud623@gmail.com>
2024-05-28 17:30:25 +0900

    SearchFragment 에서 검색 기능 구현

--- app/src/main/java/com/keunyoung/rxproject/SearchFragment.kt
+++ app/src/main/java/com/keunyoung/rxproject/SearchFragment.kt
@@ -4,35 +4,66 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.keunyoung.rxproject.databinding.FragmentSearchBinding
 import com.keunyoung.rxproject.list.ListAdapter
+import com.keunyoung.rxproject.repository.SearchRepository
+import com.keunyoung.rxproject.repository.SearchRepositoryImpl
 
 class SearchFragment : Fragment() {
-	private var binding: FragmentSearchBinding? = null
+	private var _binding: FragmentSearchBinding? = null
+	private val binding get() = _binding!!
+	private val viewModel: SearchViewModel by viewModels {
+		SearchViewModel.SearchViewModelFactory(SearchRepositoryImpl(RetrofitManager.searchService))
+	}
 	private val adapter by lazy { ListAdapter() }
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
 	): View {
 		return FragmentSearchBinding.inflate(inflater, container, false).apply {
-			binding = this
+			_binding = this
+			lifecycleOwner = viewLifecycleOwner
+			viewModel = this@SearchFragment.viewModel
 		}.root
 	}
 	
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		binding?.apply {
-			recyclerView.adapter = adapter
-		}
+		initView()
+		initViewModel()
 	}
 	
 	override fun onDestroyView() {
 		super.onDestroyView()
-		binding = null
+		_binding = null
 	}
 	
-	fun searchKeyword(text: String){
-	
+	private fun initView() {
+		binding.apply {
+			recyclerView.adapter = adapter
+		}
 	}
+	
+	private fun initViewModel() {
+		viewModel.listLiveData.observe(viewLifecycleOwner) { listItems ->
+			binding.apply {
+				if (listItems.isEmpty()) {
+					emptyTextView.isVisible = true
+					recyclerView.isVisible = false
+				} else {
+					emptyTextView.isVisible = false
+					recyclerView.isVisible = true
+				}
+				adapter.submitList(listItems)
+			}
+		}
+	}
+	
+	fun searchKeyword(text: String) {
+		viewModel.search(text)
+	}
+	
 }
