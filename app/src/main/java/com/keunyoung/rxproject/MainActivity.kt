fbac30c11d60a00453011f21c79f484a10ab9b35
LB-Brandon <rmsdud623@gmail.com>
2024-05-28 13:07:51 +0900

    MainActivity 에 ViewPager 연결

--- app/src/main/java/com/keunyoung/rxproject/MainActivity.kt
+++ app/src/main/java/com/keunyoung/rxproject/MainActivity.kt
@@ -4,17 +4,35 @@ import android.os.Bundle
 import android.view.Menu
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.keunyoung.rxproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 	
 	private lateinit var binding: ActivityMainBinding
 	
+	private val searchFragment = SearchFragment()
+	private val fragmentList = listOf(searchFragment, FavoritesFragment())
+	private val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle, fragmentList)
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		initView()
+	}
+	
+	private fun initView() {
 		binding = ActivityMainBinding.inflate(layoutInflater).apply {
 			setContentView(root)
 			view = this@MainActivity
+			viewPager.adapter = adapter
+			
+			TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+				tab.text = if (fragmentList[position] is SearchFragment) {
+					"검색 결과"
+				} else {
+					"즐겨찾기"
+				}
+			}.attach()
 		}
 	}
 	
