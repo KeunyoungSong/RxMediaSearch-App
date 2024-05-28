c081de72620edead16f8a6a583c8b349e55df86a
LB-Brandon <rmsdud623@gmail.com>
2024-05-28 16:49:48 +0900

    Kakao api 를 사용한 SearchService 구현

---
+++ app/src/main/java/com/keunyoung/rxproject/SearchService.kt
@@ -0,0 +1,20 @@
+package com.keunyoung.rxproject
+
+import android.database.Observable
+import com.keunyoung.rxproject.model.ImageListResponse
+import com.keunyoung.rxproject.model.VideoListResponse
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface SearchService {
+	
+	@Headers("Authorization: KakoAK b2613969061fe6dfb5d6ac25dbdba9dd")
+	@GET("image")
+	fun searchImage(@Query("query") query: String): Observable<ImageListResponse>
+	
+	@Headers("Authorization: KakoAK b2613969061fe6dfb5d6ac25dbdba9dd")
+	@GET("video")
+	fun searchVideo(@Query("query") query: String): Observable<VideoListResponse>
+	
+}
