b16a28793da298aab29248944da2bc44fa54b5a6
LB-Brandon <rmsdud623@gmail.com>
2024-05-28 18:53:42 +0900

    아이템 이벤트 핸들러를 통해 추가된 좋아요 목록 FavoritesFragment 에 리스트로 띄우는 기능 구현

--- app/src/main/java/com/keunyoung/rxproject/FavoritesFragment.kt
+++ app/src/main/java/com/keunyoung/rxproject/FavoritesFragment.kt
@@ -4,32 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.keunyoung.rxproject.databinding.FragmentFavoritesBinding
 import com.keunyoung.rxproject.list.ListAdapter
 
 class FavoritesFragment : Fragment() {
-	private var binding: FragmentFavoritesBinding? = null
+	private var _binding: FragmentFavoritesBinding? = null
+	private val binding get() = _binding!!
 	private val adapter by lazy { ListAdapter() }
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
 	): View {
 		return FragmentFavoritesBinding.inflate(inflater, container, false).apply {
-			binding = this
+			_binding = this
 		}.root
 	}
 	
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		binding?.apply {
+		binding.apply {
 			recyclerView.adapter = adapter
 		}
 	}
 	
+	override fun onResume() {
+		super.onResume()
+		with(binding) {
+			if (Common.favoritesList.isEmpty()) {
+				emptyTextView.isVisible = true
+				recyclerView.isVisible = false
+			} else {
+				emptyTextView.isVisible = false
+				recyclerView.isVisible = true
+			}
+		}
+		adapter.submitList(Common.favoritesList.sortedBy { it.dateTime })
+	}
 	
 	override fun onDestroyView() {
 		super.onDestroyView()
-		binding = null
+		_binding = null
 	}
 }
